074f04350da8baf64da5a76f1a7787510333d7a1
Jvlima22 <josulima90@gmail.com>
2025-11-13 10:41:10 -0300

    Remove deprecated trainingScreen.tsx from src/screens/App

--- src/screens/App/trainingScreen.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Text } from "react-native";
-
-const TrainingScreen: React.FC = () => {
-  return <Text>Training Screen</Text>;
-};
-
-export default TrainingScreen;
